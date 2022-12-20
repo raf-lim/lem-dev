3d07851e787f93f8774f9ec7ed2d09cf1529faa6
raf-lim <raf.sukiennik@gmail.com>
2022-12-20 16:04:15 +0100

    add temporary view to views.py in news app

--- backend/apps/news/views.py
+++ backend/apps/news/views.py
@@ -1,3 +1,9 @@
 # from django.shortcuts import render
 
-# Create your views here.
+# Temporary import
+from django.shortcuts import HttpResponse
+
+
+# Temporary view
+def welcome():
+    return HttpResponse("<h3>Welcome to news cms</h3>")
