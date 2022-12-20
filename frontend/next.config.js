9929d7090dec8d732e207e60348cd1292365fb1d
aruzo <dawid12391111@gmail.com>
2022-12-20 19:31:09 +0100

    Configured development docker (#8)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
 
-module.exports = nextConfig
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
