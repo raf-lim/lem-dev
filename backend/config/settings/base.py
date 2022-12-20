4641c933c44ca47e107f5a631c9397a2e2ec90d8
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2022-12-20 21:18:11 +0100

    Change urls and base files

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -64,7 +64,7 @@ THIRD_PARTY_APPS = [
     "drf_spectacular",
 ]
 
-LOCAL_APPS = ["apps.users", "apps.forum", "apps.news", "apps.books"]
+LOCAL_APPS = ["apps.users", "apps.forum", "apps.news", "apps.books", "apps.likes"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
