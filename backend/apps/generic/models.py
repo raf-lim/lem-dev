b7ca96618ee156dce48aef20b09a4cc8b8553f67
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2022-12-25 23:56:09 +0100

    Add like type and counter function

--- backend/apps/generic/models.py
+++ backend/apps/generic/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -11,14 +11,22 @@ class Like(models.Model):
     Fields:
         user (models.ForeignKey): Foreign key to the User who made the like.
         content_type (models.ForeignKey): Foreign key to the ContentType of the liked content.
-        object_id (models.PositiveIntegerField): Primary key of the liked content.
+        object_id (models.PositiveIntegerField): Primary key of the liked content.po
         content_object (GenericForeignKey): Generic foreign key to the liked content.
     """
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+
+    LIKE = "L"
+    DISLIKE = "D"
+    TYPE_CHOICES = [
+        (LIKE, "Like"),
+        (DISLIKE, "Dislike"),
+    ]
+    like_type = models.CharField(max_length=1, choices=TYPE_CHOICES)
 
     class Meta:
         """
@@ -28,3 +36,15 @@ class Like(models.Model):
         ordering = ["user"]
         verbose_name = "Like"
         verbose_name_plural = "Likes"
+
+    def like_dislike_counter(self, content_type, object_id, like_type, user):
+        # Get the content type instance for the given content type
+        ct = ContentType.objects.get(model=content_type)
+
+        # Get all the likes or dislikes for the given content type, object id, and user
+        likes_dislikes = Like.objects.filter(
+            content_type=ct, object_id=object_id, like_type=like_type, user=user
+        )
+
+        # Count the number of likes or dislikes and return the result
+        return likes_dislikes.count()
