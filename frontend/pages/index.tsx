c3c446ef4afea26e89a200c70c64aa0f76b8f058
Indoxer <indoxer.mk@gmail.com>
2022-12-29 16:55:28 +0100

    Add Jest

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
-
 const Home = () => {
-  const [state, setState] = useState(1);
-  return (
-    <>
-      <p>{state}</p>
-      <button onClick={() => setState(state + 1)}>Click me!</button>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
