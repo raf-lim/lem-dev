7a26ef95822af145c7e23f91ffbb78047c962067
Indoxer <indoxer.mk@gmail.com>
2022-12-29 15:43:37 +0100

    Add cypress

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <></>;
+  const [state, setState] = useState(1);
+  return (
+    <>
+      <p>{state}</p>
+      <button onClick={() => setState(state + 1)}>Click me!</button>
+    </>
+  );
 };
 
 export default Home;
