c44e0d90ee1cada505e67e4598be82fc9f6fbabe
raf-lim <raf.sukiennik@gmail.com>
2022-12-30 00:44:01 +0100

    add router to urls in news app

--- backend/apps/news/urls.py
+++ backend/apps/news/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+"""News app urls."""
+
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-# temporary
-app_name = "news"
+router = DefaultRouter()
+
+router.register("news", views.NewsViewSet)
+router.register("comments", views.CommentViewSet)
+router.register("tags", views.TagViewSet)
+router.register("files", views.FileViewSet)
+
 urlpatterns = [
-    path("", views.welcome, name="welcome"),
+    path("", include(router.urls)),
+    # temporary:
+    path("welcome/", views.welcome, name="welcome"),
 ]
