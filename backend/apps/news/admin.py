b25ed0e8a56e0ffd7971adef4351d3ca3511b7bb
raf-lim <raf.sukiennik@gmail.com>
2022-12-30 01:36:32 +0100

    news app models registered in admin

--- backend/apps/news/admin.py
+++ backend/apps/news/admin.py
@@ -1,3 +1,41 @@
-# from django.contrib import admin
+"""News app admin registered models."""
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Comment, File, Highlight, Image, Like, News, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["pk", "tag", "content_object", "user"]
+    search_fields = ["tag"]
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ["pk", "file", "content_object", "user"]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["pk", "image", "content_object", "user"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["pk", "body", "content_object", "user"]
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["pk", "title", "is_published", "user"]
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ["pk", "like", "content_object", "user"]
+
+
+@admin.register(Highlight)
+class Highlightdmin(admin.ModelAdmin):
+    list_display = ["pk", "highlight", "content_object", "user"]
