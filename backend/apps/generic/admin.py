079a4189068b128494bad89f213485dd353125b1
Indoxer <indoxer.mk@gmail.com>
2023-01-09 21:10:25 +0100

    I remove the admin for this moment, but we will do it later

--- backend/apps/generic/admin.py
+++ backend/apps/generic/admin.py
@@ -1,8 +1,1 @@
-from django.contrib import admin
-
-from apps.generic.models import Reaction
-
-
-@admin.register(Reaction)
-class ReactionAdmin(admin.ModelAdmin):
-    readonly_fields = ("content_object",)
+# from django.contrib import admin
