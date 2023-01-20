3b42717faf4a4dae79f7fb4a973935090cd78d63
raf-lim <raf.sukiennik@gmail.com>
2023-01-20 01:11:05 +0100

    add Author model

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,5 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.fields import (
+    AutoSlugField,
+    CreationDateTimeField,
+    ModificationDateTimeField,
+)
 
 
 class User(AbstractUser):
     pass
+
+
+# temporary here, move to books/models if available?
+class Author(models.Model):
+    """Model for books' authors."""
+
+    class Meta:
+        verbose_name = _("author")
+        verbose_name_plural = _("authors")
+
+    # TODO get it done.
+
+    def __str__(self) -> str:
+        return self.full_name
