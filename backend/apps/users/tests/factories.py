bc0528b63516ff5b48edad2a226670704297db17
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2023-01-23 22:27:20 +0100

    Update UserFactory in users app (#67)
    * Convert factory.faker to faker, added lazyfunction
    
    * Fixed error with faker
    
    * Fixed error with faker

--- backend/apps/users/tests/factories.py
+++ backend/apps/users/tests/factories.py
@@ -1,9 +1,13 @@
-from collections.abc import Sequence
-from typing import Any
-
 from django.contrib.auth import get_user_model
-from factory import Faker, post_generation
+from django.contrib.auth.hashers import make_password
+from factory import LazyFunction
 from factory.django import DjangoModelFactory
+from faker import Faker
+from faker.providers import internet, misc
+
+faker = Faker()
+faker.add_provider(internet)
+faker.add_provider(misc)
 
 
 class UserFactory(DjangoModelFactory):
@@ -11,23 +15,8 @@ class UserFactory(DjangoModelFactory):
         model = get_user_model()
         django_get_or_create = ["username"]
 
-    username = Faker("user_name")
-    email = Faker("email")
-    first_name = Faker("first_name")
-    last_name = Faker("last_name")
-
-    @post_generation
-    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        password = (
-            extracted
-            if extracted
-            else Faker(
-                "password",
-                length=42,
-                special_chars=True,
-                digits=True,
-                upper_case=True,
-                lower_case=True,
-            ).evaluate(None, None, extra={"locale": None})
-        )
-        self.set_password(password)
+    username = LazyFunction(lambda: faker.user_name())
+    email = LazyFunction(lambda: faker.email())
+    first_name = LazyFunction(lambda: faker.first_name())
+    last_name = LazyFunction(lambda: faker.last_name())
+    password = LazyFunction(lambda: make_password(faker.password()))
