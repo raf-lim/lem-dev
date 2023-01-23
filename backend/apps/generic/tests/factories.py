a304d000606ebd6573feaeba8e67a0ba37d7b818
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-01-23 22:58:09 +0100

    Fix ReactionUserFactory reaction_type error

--- backend/apps/generic/tests/factories.py
+++ backend/apps/generic/tests/factories.py
@@ -1,56 +1,45 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from factory import LazyAttribute, LazyFunction, SelfAttribute, SubFactory
 from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyChoice
 from faker import Faker
 
 from apps.generic.models import Reaction
+from apps.users.tests.factories import UserFactory
 
 faker = Faker()
 
 
-class UserFactory(DjangoModelFactory):
-    class Meta:
-        model = get_user_model()
-        django_get_or_create = ["username"]
-
-    username = LazyFunction(lambda: faker.user_name())
-    email = LazyFunction(lambda: faker.email())
-    first_name = LazyFunction(lambda: faker.first_name())
-    last_name = LazyFunction(lambda: faker.last_name())
-
-
 class GroupFactory(DjangoModelFactory):
-    name = LazyFunction(lambda: faker.name())
-
     class Meta:
         model = Group
 
+    name = LazyFunction(lambda: faker.name())
+
 
 class ReactionFactory(DjangoModelFactory):
-    object_id = SelfAttribute("content_object.id")
-    content_type = LazyAttribute(
-        lambda o: ContentType.objects.get_for_model(o.content_object)
-    )
-    reaction_type = LazyFunction(
-        lambda: faker.random_choices(elements=Reaction.TYPE_CHOICES)
-    )
-
     class Meta:
         exclude = ["content_object"]
         abstract = True
 
+    user = SubFactory(UserFactory)
+    object_id = SelfAttribute("content_object.id")
+    content_type = LazyAttribute(
+        lambda o: ContentType.objects.get_for_model(o.content_object)
+    )
+    reaction_type = FuzzyChoice(choices=Reaction.TYPE_CHOICES, getter=lambda c: c[0])
+
 
 class ReactionUserFactory(ReactionFactory):
-    content_object = SubFactory(UserFactory)
-
     class Meta:
         model = Reaction
+
+    content_object = SubFactory(UserFactory)
 
 
 class ReactionGroupFactory(ReactionFactory):
-    content_object = SubFactory(GroupFactory)
-
     class Meta:
         model = Reaction
+
+    content_object = SubFactory(GroupFactory)
